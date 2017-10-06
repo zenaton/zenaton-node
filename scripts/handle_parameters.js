c43638c77360c30934065021f632634ec71774dc
Louis Cibot <louiscibot@gmail.com>
2017-10-06 15:36:01 +0200

    update handle_parameters script

--- scripts/handle_parameters.js
+++ scripts/handle_parameters.js
@@ -1,8 +1,6 @@
 var HandleParameters = require('../lib/Worker/HandleParameters');
-var classes = process.argv[1];
-var boot = process.argv[2];
-
+var classes = process.argv[2];
+var boot = process.argv[3];
 require(boot);
-
 const response = (new HandleParameters()).process(classes);
 console.log(JSON.stringify(response));
