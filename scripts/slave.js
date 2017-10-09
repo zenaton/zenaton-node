86c37b59cf3b05e091f85c73537df36e6171f4a5
Louis Cibot <louiscibot@gmail.com>
2017-10-09 22:03:56 +0200

    delete instanceID

--- scripts/slave.js
+++ scripts/slave.js
@@ -1,10 +1,9 @@
 var Slave = require('../lib/Worker/Slave');
 var boot = process.argv[2];
-var instanceId = process.argv[3];
-var slaveId = process.argv[4];
+var slaveId = process.argv[3];
 require('../lib/functions');
 require(boot);
 
 
 // arg3: instance_id, arg4: slave_id
-(new Slave(instanceId, slaveId)).process();
+(new Slave(slaveId)).process();
