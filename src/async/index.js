17250579a4cc6da81ae803bf34b0d4bc6aabac4e
Gilles Barbier <gilles@zenaton.com>
2019-08-17 21:26:44 +0200

    restire array parallel for async

--- src/async/index.js
+++ src/async/index.js
@@ -5,6 +5,30 @@ const { Workflow, workflowManager, Version } = require("./Workflows");
 const { serializer } = require("./Services");
 const Parallel = require("./Parallel/Parallel");
 
+// if below functions are already defined, use Parallel class
+
+// Parallel dispatchs
+if (!Array.prototype.dispatch) {
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.dispatch = function dispatch() {
+    console.warn(
+      "DeprecationWarning: Usage of '[a, b, c].dispatch()' is deprecated.\nPlease use 'new Parallel(a, b, c).dispatch()' instead.",
+    );
+    return new Engine().dispatch(this);
+  };
+}
+
+// Parallel executions
+if (!Array.prototype.execute) {
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.execute = function execute() {
+    console.warn(
+      "DeprecationWarning: Usage of '[a, b, c].execute()' is deprecated.\nPlease use 'new Parallel(a, b, c).execute()' instead.",
+    );
+    return new Engine().execute(this);
+  };
+}
+
 module.exports = {
   Client,
   Engine,
