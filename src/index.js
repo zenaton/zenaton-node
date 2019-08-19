081c2195e4503ce3b9d8bb2cfa1f64a41f60362f
Gilles Barbier <gilles@zenaton.com>
2019-08-20 00:55:09 +0200

    typo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 const path = require("path");
 const { readdirSync, statSync } = require("fs");
+const { version } = require("./infos");
+const Errors = require("./Errors");
 
 const getDirectories = (source) => readdirSync(source);
 
 // store path to this file for use by Zenaton worker
 process.env.ZENATON_LIBRARY_PATH = path.resolve(__dirname, __filename);
 
-const { version } = require("./infos");
-const Errors = require("./Errors");
-
-const lastCodePath = "201908_draft";
-
 // Helping the Agent to check if is able to manage this version
+const lastCodePath = "201908_draft";
 process.env.ZENATON_LAST_CODE_PATH = `${lastCodePath}`;
-
 // eslint-disable-next-line import/no-dynamic-require
 const pathLast = require(`./Code/${lastCodePath}`);
 
-// last version
+// Define main version
 const index = {
   Errors,
   infos: {
