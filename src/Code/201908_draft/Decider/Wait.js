529c78014099103428529fddafb80acb5c31e4a8
Gilles Barbier <gilles@zenaton.com>
2019-08-19 21:26:50 +0200

    Recompose Code Paths

---
+++ src/Code/201908_draft/Decider/Wait.js
@@ -0,0 +1,87 @@
+const uuidv4 = require("uuid/v4");
+const InvalidArgumentError = require("../../../Errors/InvalidArgumentError");
+
+const Wait = class Wait {
+  constructor(processor) {
+    this._processor = processor;
+    this.eventName = null;
+    this.timestamp = null;
+    this.duration = null;
+
+    return this;
+  }
+
+  set processor(processor) {
+    this._processor = processor;
+    return this;
+  }
+
+  forever() {
+    this.duration = null;
+
+    return this._apply();
+  }
+
+  for(duration) {
+    if (!Number.isInteger(duration)) {
+      if (
+        typeof duration !== "object" ||
+        duration.constructor.name !== "Duration"
+      ) {
+        throw new InvalidArgumentError(
+          `Parameter of "wait.for()" must be an integer or a "Duration" object`,
+        );
+      }
+    }
+
+    this.duration = Number.isInteger(duration)
+      ? duration
+      : duration._getDefinition();
+
+    return this._apply();
+  }
+
+  until(timestamp) {
+    if (!Number.isInteger(timestamp)) {
+      if (
+        typeof timestamp !== "object" ||
+        timestamp.constructor.name !== "DateTime"
+      ) {
+        throw new InvalidArgumentError(
+          `Parameter of "wait.until()" must be a timestamp or a "DateTime" object`,
+        );
+      }
+    }
+
+    this.timestamp = Number.isInteger(timestamp)
+      ? timestamp
+      : timestamp._getDefinition();
+
+    return this._apply();
+  }
+
+  event(eventName) {
+    this.eventName = eventName;
+
+    return this;
+  }
+
+  async _apply() {
+    return this._processor._executeTask(this._getWait());
+  }
+
+  _getWait() {
+    return {
+      type: "wait",
+      name: "_Wait",
+      input: {
+        event: this.eventName,
+        duration: this.duration,
+        timestamp: this.timestamp,
+      },
+      intentId: uuidv4(),
+    };
+  }
+};
+
+module.exports = Wait;
---
+++ src/Code/201908_draft/Services/Objectify.js
@@ -0,0 +1,20 @@
+const { InternalZenatonError } = require("../../../Errors");
+
+const objectify = function objectify(Class) {
+  if (typeof Class !== "function") {
+    throw new InternalZenatonError(
+      `Parameter of "objectify" must be a function, not a "${typeof Class}"`,
+    );
+  }
+  // copy public methods to provide a new instance with this method applied
+  const obj = {};
+  Object.getOwnPropertyNames(Class.prototype).forEach((method) => {
+    if (method !== "constructor" && !method.startsWith("_")) {
+      obj[method] = (...args) => new Class()[method](...args);
+    }
+  });
+
+  return obj;
+};
+
+module.exports = objectify;
