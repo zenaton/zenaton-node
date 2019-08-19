c0bffa13583bab4527f1d0bb64b9444d4bbc5510
Julien <julien@zenaton.com>
2019-08-19 11:24:36 +0200

    rename attemptIndex

--- src/async/Runtime/Contexts/TaskContext.js
+++ src/async/Runtime/Contexts/TaskContext.js
@@ -1,6 +1,6 @@
 module.exports = class TaskContext {
   constructor(values = {}) {
-    this.attemptIndex = values.attemptIndex || null;
+    this.retryIndex = values.retryIndex || null;
     this.id = values.id || null;
   }
 };
