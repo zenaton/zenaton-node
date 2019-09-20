f8a91ace57d9c2373d9b1d01a535a6e21e7cc196
Julien <julien@zenaton.com>
2019-09-20 16:16:11 +0200

    fix duration and datetime get method for null

--- src/Code/201908_draft/Services/DateTime.js
+++ src/Code/201908_draft/Services/DateTime.js
@@ -178,6 +178,8 @@ class DateTime {
   }
 
   get(definition, baseDate) {
+    if (definition === null) return null;
+
     const timeDefinition = definition || this._getDefinition();
 
     if (Number.isInteger(timeDefinition)) {
