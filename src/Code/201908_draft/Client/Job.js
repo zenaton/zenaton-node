c8e512b7a2f7a8ae650a9afc7ebe98183789b0e2
Gilles Barbier <gilles@zenaton.com>
2019-09-23 00:11:21 +0200

    simplify Job

--- src/Code/201908_draft/Client/Job.js
+++ src/Code/201908_draft/Client/Job.js
@@ -1,7 +1,5 @@
 module.exports = class Job {
   constructor(values = {}) {
     this.id = values.id || null;
-    this.appId = values.appId || null;
-    this.appEnv = values.appEnv || null;
   }
 };
