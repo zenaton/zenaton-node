8e04681a7c78850172fbeb780c1e22e1919345e4
Gilles Barbier <gilles@zenaton.com>
2019-09-23 13:47:56 +0200

    add http connector

--- src/Code/201908_draft/Client/Connector.js
+++ src/Code/201908_draft/Client/Connector.js
@@ -1,48 +1,79 @@
 const uuidv4 = require("uuid/v4");
-const Run = require("./Run");
-const objectify = require("../Services/Objectify");
 const { ExternalZenatonError } = require("../../../Errors");
 
 const Connector = class Connector {
   constructor(service, serviceId, processor) {
     this._checkString(service, "First", "connector's name");
-    this._checkString(serviceId, "Second", "connector's id");
+    if (service !== "http") {
+      this._checkString(serviceId, "Second", "connector's id");
+    }
 
     this._service = service;
     this._serviceId = serviceId;
     this._processor = processor;
   }
 
-  get run() {
-    return objectify(Run, this._processor, this._service, this._serviceId);
+  post(url, body, header) {
+    return this._http("post", url, body, header);
+  }
+
+  get(url, body, header) {
+    return this._http("get", url, body, header);
+  }
+
+  put(url, body, header) {
+    return this._http("put", url, body, header);
+  }
+
+  patch(url, body, header) {
+    return this._http("patch", url, body, header);
+  }
+
+  delete(url, body, header) {
+    return this._http("delete", url, body, header);
+  }
+
+  _http(verb, url, body, headers) {
+    if (!this._processor.runTask) {
+      throw new ExternalZenatonError(
+        `Sorry, you can not use "${this._service}.${verb}" syntax from here`,
+      );
+    }
+    return this._processor.runTask(this._getJob(verb, url, body, headers));
   }
 
   _getJob(verb, url, body, headers) {
-    this._checkString(url, "First", "url", `.${verb}`);
+    this._checkString(url, "First", "url", verb);
 
     return {
-      name: `${verb} ${this._service}:${url}`,
-      input: {
-        service: this._service,
-        serviceId: this._serviceId,
-        verb,
-        url,
-        body,
-        headers,
-      },
+      name: `${this._service}:${verb}`,
+      input: [
+        {
+          service: this._service,
+          serviceId: this._serviceId,
+          verb,
+          url,
+          body,
+          headers,
+        },
+      ],
       intentId: uuidv4(),
     };
   }
 
-  _checkString(val, position, type, method = "") {
+  _checkString(val, position, type, verb) {
     if (typeof val !== "string") {
       throw new ExternalZenatonError(
-        `${position} parameter of "connector${method}" must be a string (${type}) - not a "${typeof id}"`,
+        `${position} parameter of "${
+          this._service
+        }.${verb}" must be a string (${type}) - not a "${typeof id}"`,
       );
     }
     if (typeof val !== "string" || val.length > 128) {
       throw new ExternalZenatonError(
-        `${position} parameter of "connector${method}" (${type}) must have less than 128 characters"`,
+        `${position} parameter of "${
+          this._service
+        }.${verb}" (${type}) must have less than 128 characters"`,
       );
     }
   }
